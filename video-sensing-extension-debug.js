c04a2e3f8f801dd39f2a9121381ea3440ca07738
Fatih Akca <fatihakca5@gmail.com>
2018-12-22 15:22:22 +0000

    Build for b8456ea32aa6f1886a58c274c2b51fb6e45aeb38

--- video-sensing-extension-debug.js
+++ video-sensing-extension-debug.js
@@ -82,7 +82,7 @@ var VirtualMachine =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 482);
+/******/ 	return __webpack_require__(__webpack_require__.s = 479);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -205,15 +205,15 @@ module.exports = {
 
 /***/ }),
 
-/***/ 482:
+/***/ 479:
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function(global) {module.exports = global["Scratch3VideoSensingDebug"] = __webpack_require__(483);
+/* WEBPACK VAR INJECTION */(function(global) {module.exports = global["Scratch3VideoSensingDebug"] = __webpack_require__(480);
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(4)))
 
 /***/ }),
 
-/***/ 483:
+/***/ 480:
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
@@ -223,7 +223,7 @@ module.exports = {
  */
 
 const VideoMotion = __webpack_require__(87);
-const VideoMotionView = __webpack_require__(484);
+const VideoMotionView = __webpack_require__(481);
 
 module.exports = {
     VideoMotion,
@@ -233,7 +233,7 @@ module.exports = {
 
 /***/ }),
 
-/***/ 484:
+/***/ 481:
 /***/ (function(module, exports, __webpack_require__) {
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
