221404ea47527a106b4b11d954a55bddf424d388
Yağmur Oymak <yagmur.oymak@gmail.com>
2019-04-26 14:42:09 +0300

    Return the dict object to the GUI monitor
    Instead of formatting it here (turning it into an array), we return
    the dictionary object as it is and the GUI will handle the rest.

--- src/blocks/scratch3_data.js
+++ src/blocks/scratch3_data.js
@@ -264,23 +264,19 @@ class Scratch3DataBlocks {
         const dict = util.target.lookupOrCreateDict(
             args.DICT.id, args.DICT.name);
 
+        // If block is running for monitors, return copy of dict if changed.
         if (util.thread.updateMonitor) {
-            // Return original array representation if up-to-date, which doesn't trigger monitor update.
-            if (dict._monitorUpToDate) return dict.arrayRepr;
-            // If value changed, reset the flag, update the stored array and return a copy to trigger monitor update.
+            // Return original value if up-to-date, which doesn't trigger monitor update.
+            if (dict._monitorUpToDate) return dict.value;
+            // If value changed, reset the flag and return a copy to trigger monitor update.
             // Because monitors use Immutable data structures, only new objects trigger updates.
             dict._monitorUpToDate = true;
-            // TODO: Make this prettier together with the help of bb-gui
-            dict.arrayRepr = Object.keys(dict.value);
-            dict.arrayRepr = dict.arrayRepr.map(key => `${key}:${dict.value[key]}`);
-            return dict.arrayRepr;
+            return JSON.parse(JSON.stringify(dict.value));
         }
 
-        let result = '';
-        for (const key in dict.value) {
-            result = `${result}${key}:${dict.value[key]} `;
-        }
-        return result;
+        return Object.keys(dict.value)
+            .map(key => `${key}:${dict.value[key]}`)
+            .join(' ');
     }
 
     addToDict (args, util) {
